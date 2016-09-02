1a12c0c5c25b235de33509f659ff152dae824aa5
Unknown6656 <unknown84@gmx.de>
2016-09-02 17:39:48 +0200

    Added BinaryStream test

--- Tests/Cosmos.Kernel.Tests.System.IO/Kernel.cs
+++ Tests/Cosmos.Kernel.Tests.System.IO/Kernel.cs
@@ -4,62 +4,129 @@ using System.Collections.Generic;
 using System.Text;
 using Cosmos.TestRunner;
 
+using static System.ConsoleColor;
+
 using Sys = Cosmos.System;
 
 namespace Cosmos.Kernel.Tests.System.IO
 {
-    public unsafe delegate void TestDelegate(Kernel k, int* hresult);
+    public unsafe delegate void TestDelegate(Kernel k, long* hresult);
 
-    public class Kernel
+    public unsafe class Kernel
         : Sys.Kernel
     {
         // Invoke them manually, as reflection is not (yet) provided
         internal static readonly TestDelegate[] functions = new TestDelegate[] {
-
+            TestMethods.BinaryStreamTest
         };
+
         
         protected override void BeforeRun()
         {
             Console.WriteLine("Testing Operating System for the namespace '[mscorlib.dll]global::System.IO.*'");
         }
 
-        protected unsafe override void Run()
+        protected override void Run()
         {
-            try
-            {
-                int hresult = 0;
+            Console.ForegroundColor = White;
+            Console.WriteLine("\nPress (T) to start the tests or any other key to shut down the machine.");
 
-                for (int i = 0, l = functions.Length; i < l; i++)
+            char c = Console.ReadKey(true).KeyChar;
+
+            if (c == 't' || c == 'T')
+                try
                 {
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.WriteLine($"Testing method {i + 1}/{l + 1} ...");
-                    Console.ForegroundColor = ConsoleColor.White;
+                    long hresult = 0;
+                    int total_err = 0;
+                    int l = functions.Length;
 
-                    functions[i].Invoke(this, &hresult);
+                    for (int i = 0; i < l; i++)
+                    {
+                        Console.ForegroundColor = Cyan;
+                        Console.WriteLine($"Testing method {i + 1}/{l} ...");
+                        Console.ForegroundColor = White;
 
-                    if (hresult != 0)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"Method Test {i + 1}/{l + 1} failed with the return code {hresult}.");
+                        hresult = 0;
+                        functions[i].Invoke(this, &hresult);
+
+                        if (hresult != 0)
+                        {
+                            Console.ForegroundColor = Red;
+                            Console.WriteLine($"\nMethod Test {i + 1}/{l} failed with the return code {hresult} (0x{hresult:x16}).");
+
+                            total_err++;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = Green;
+                            Console.WriteLine($"\nMethod Test {i + 1}/{l} succesful.");
+                        }
                     }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine($"Method Test {i + 1}/{l + 1} succesful.");
-                    }
+
+                    Console.ForegroundColor = total_err == 0 ? Green : Red;
+                    Console.WriteLine($"{l - total_err}/{l} Tests have been successfull, {total_err} Errors occured.");
                 }
-            }
-            catch (Exception ex)
-            {
-                mDebugger.Send("Exception occurred: " + ex.Message);
+                catch (Exception ex)
+                {
+                    mDebugger.Send("Exception occurred: " + ex.Message);
 
-                TestController.Failed();
-            }
+                    Console.ForegroundColor = Red;
+                    Console.WriteLine("\nFatal error occured.");
+
+                    TestController.Failed();
+                }
+            else
+                this.Stop();
         }
     }
 
-    public static class TestMethods
+    public static unsafe class TestMethods
     {
+        /// <summary>
+        /// Tests the classes "System::IO::BinaryReader" and "System::IO::BinaryWriter"
+        /// </summary>
+        public static void BinaryStreamTest(Kernel k, long* hresult)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            using (BinaryReader rd = new BinaryReader(ms))
+            using (BinaryWriter rw = new BinaryWriter(ms))
+            {
+                // write methods
+                rw.Write(true);
+                rw.Write((byte)0x42);
+                rw.Write('U');
+                rw.Write("Unknown6656");
+                rw.Write(4.2m);
+                rw.Write(4.2f);
+                rw.Write(4.2d);
+                rw.Write(42);
+                rw.Write(42U);
+                rw.Write(42L);
+                rw.Write(42UL);
+                rw.Write(new byte[3] { 0x42, 0x31, 0x56 });
+                
+                long flag = 1L;
+                Action<bool> writeresult = new Action<bool>(_ => *hresult |= _ ? 0 : (flag <<= 1));
 
+                // read methods
+                writeresult(rd.ReadBoolean());
+                writeresult(rd.ReadByte() == 0x42);
+                writeresult(rd.ReadChar() == 'U');
+                writeresult(rd.ReadString() == "Unknown6656");
+                writeresult(rd.ReadDecimal() == 4.2m);
+                writeresult(rd.ReadSingle() == 4.2f);
+                writeresult(rd.ReadDouble() == 4.2d);
+                writeresult(rd.ReadInt32() == 42);
+                writeresult(rd.ReadUInt32() == 42U);
+                writeresult(rd.ReadInt64() == 42L);
+                writeresult(rd.ReadUInt64() == 42UL);
+
+                byte[] arr = rd.ReadBytes(3);
+
+                writeresult((arr[0] == 0x42) &&
+                            (arr[1] == 0x31) &&
+                            (arr[2] == 0x56));
+            }
+        }
     }
 }
