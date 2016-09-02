e78fa9800ce611f46a62224a0fc3159c6c82d9f3
Unknown6656 <unknown84@gmx.de>
2016-09-02 15:56:56 +0200

    Added Test Project for System.IO

---
+++ source/Cosmos.Kernel.Tests.System.IO/Kernel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using Cosmos.TestRunner;
+
+using Sys = Cosmos.System;
+
+namespace Cosmos.Kernel.Tests.System.IO
+{
+    public delegate int TestDelegate(Kernel k);
+
+    public class Kernel
+        : Sys.Kernel
+    {
+        // Invoke them manually, as reflection is not (yet) provided
+        internal static readonly TestDelegate[] functions = new TestDelegate[] {
+
+        };
+
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Testing Operating System for the namespace '[mscorlib.dll]global::System.IO.*'");
+        }
+
+        protected override void Run()
+        {
+            try
+            {
+                for (int i = 0, l = functions.Length; i < l; i++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine($"Testing method {i + 1}/{l + 1} ...");
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    int hresult = functions[i].Invoke(this);
+
+                    if (hresult != 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Method Test {i + 1}/{l + 1} failed with the return code {hresult}.");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Method Test {i + 1}/{l + 1} succesful.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                mDebugger.Send("Exception occurred: " + ex.Message);
+
+                TestController.Failed();
+            }
+        }
+    }
+
+    public static class TestMethods
+    {
+
+    }
+}
